7a89a91eb8f894f0daf51b1b1f61da6d40032b7d
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-06-05 08:41:53 +0530

    Update for state

--- irise-frontend/src/components/Mens.jsx
+++ irise-frontend/src/components/Mens.jsx
@@ -1,8 +1,31 @@
-
+import { useState } from "react";
 
 const Mens = () => {
+    const [number, setNumber] = useState(9876);
+    const [isLiked, setIsLiked] = useState(true);
+    // console.log(number, "number")
+
+    function counter() {
+        setNumber((prevState) => prevState + 1)
+    }
+
+    function decrement() {
+        setNumber((prevState) => prevState - 1)
+    }
+
+    function like() {
+        setIsLiked(false)
+        alert("Liked")
+    }
     return (
-        <div>Mens</div>
+        <div>
+            <h1>Mens</h1>
+            <h3>Counter : {number}</h3>
+            <button onClick={counter} >+ </button>
+            <button onClick={decrement}>-</button>
+            {isLiked ? <h2>Not Liked!</h2> : <h2>Liked</h2>}
+            <button onClick={like}>Like</button>
+        </div>
     )
 }
 
